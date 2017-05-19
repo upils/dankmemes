210adac0fa7c778a66f86f6eb560c767029c80c1
upils <paulmars@orange.fr>
2017-05-19 10:29:11 -0400

    Add a docker prune command to clean old images.

--- dev-build.sh
+++ dev-build.sh
@@ -1,4 +1,6 @@
 echo -e "\e[32m[+] Building the image\e[0m"
 docker-compose build --no-cache &&
+echo -e "\e[32m[+] Removing old useless images.\e[0m"
+docker image prune -f
 echo -e "\e[32m[+] Creating and launching the container.\e[0m"
 docker-compose up -d
