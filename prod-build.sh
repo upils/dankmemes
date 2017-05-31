c0585789040cc4a80acd09ff7e9b04a627f3a6ac
upils <paulmars@orange.fr>
2017-05-31 17:35:16 -0400

    Add a prod script and correct dev script.

--- prod-build.sh
+++ prod-build.sh
@@ -1,8 +1,8 @@
 echo -e "\e[32m[+] If it exist, stop the container\e[0m"
 docker-compose down
 echo -e "\e[32m[+] Building the image\e[0m"
-docker-compose build --no-cache
+docker-compose build
 echo -e "\e[32m[+] Removing old useless images.\e[0m"
 docker image prune -f
 echo -e "\e[32m[+] Creating and launching the container.\e[0m"
-docker-compose up
+docker-compose up -d
